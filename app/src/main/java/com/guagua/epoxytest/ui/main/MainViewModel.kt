6a0e9c8b97ee0cc8fcd52ab7b40996860c14fea1
KKSNickChou <nickchou@kkstream.com>
2020-08-25 14:47:28 +0800

    Refine to Rx coding style.

--- app/src/main/java/com/guagua/epoxytest/ui/main/MainViewModel.kt
+++ app/src/main/java/com/guagua/epoxytest/ui/main/MainViewModel.kt
@@ -12,47 +12,55 @@ import com.guagua.epoxytest.ui.extension.getSuccessBody
 import com.guagua.epoxytest.ui.main.list.ItemGroupController
 import com.guagua.epoxytest.view.data.ListItem
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.disposables.Disposable
 import io.reactivex.rxjava3.schedulers.Schedulers
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.MainScope
 import javax.inject.Inject
 
 class MainViewModel @ViewModelInject @Inject constructor(
     application: Application,
     private val repository: VideoRepository
-) : AndroidViewModel(application), ItemGroupController.AdapterCallbacks,
-    CoroutineScope by MainScope() {
+) : AndroidViewModel(application), ItemGroupController.AdapterCallbacks {
 
     private val _categories = MutableLiveData<List<Category>>()
     val categories: LiveData<List<Category>> = _categories
 
-    fun fetchCategories() {
+    private val compositeDisposable = CompositeDisposable()
+
+    fun fetchCategories() = async {
         repository.getCategoriesObservable()
             .subscribeOn(Schedulers.io())
+            .flatMapObservable {
+                val categories = it.getSuccessBody() ?: listOf()
+                _categories.postValue(categories) // Update categories title first
+                Observable.fromIterable(categories)
+            }
+            .flatMapSingle { category ->
+                repository.getVideosObservable(category.id.toString())
+                    .map {
+                        category.apply {
+                            videos = it.getSuccessBody()
+                        }
+                    }
+            }
+            .toList()
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe({
-                it.getSuccessBody() ?: return@subscribe
-
-                it.getSuccessBody()?.forEach { category ->
-                    repository.getVideosObservable(category.id.toString())
-                        .subscribeOn(Schedulers.io())
-                        .observeOn(AndroidSchedulers.mainThread())
-                        .subscribe({ response ->
-                            category.videos = response.getSuccessBody()
-
-                            // Update each category.
-                            _categories.postValue(it.body())
-                        }, { throwable ->
-                            Log.d("Nick", throwable.message)
-                        })
-                }
-
-                // Show categories title first.
-                _categories.postValue(it.body())
+                _categories.postValue(it)
             }, {})
     }
 
     override fun onListItemClick(item: ListItem) {
         Log.d("Nick", "on item click: $item")
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+
+    private fun async(disposable: () -> Disposable) {
+        compositeDisposable.add(disposable.invoke())
     }
 }
